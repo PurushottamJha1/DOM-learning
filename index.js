fb55df27b4219c1d60315785757fe08d5148b7b9
Purushottam Jha <Purushottam.J_INT1597@accolite.com>
2024-03-10 16:47:37 +0530

    Dom learning

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+// 1 body, head, documentElement
+//  console.log(document.body);
+
+
+// 2. firstElementChild (body ke bad first div) , childNodes , children  give HTML collect not Array
+// Array.from(jise bhi convert krna)
+// console.log(document.body.firstElementChild);
+
+
+// 3. getElementById 
+// console.log(document.getElementById("thisOne"));
+// output is   <div id="thisOne">how</div>
+
+
+// 4. getElemntsbyClassName
+// output is HTML collection
+// console.log(document.getElementsByClassName("one"));
+
+
+// 5. getElemntsbyname, tagName
+// return HTML collection
+
+//6
+//innerText reads text as it is rendered on screen, textContent reads text as it is in the markup.
+// innerthtml - is whole html <div> hi </div> can literally change everything
+//one[0].style.background="green"
+
+
+//querySelector(".myclass") single class
+// getAttribute gives class , setAttribute set class
+// const your=document.getElementById("your");
+// console.log(your.getAttribute("class"));
+
+
+ 
+
+
+
